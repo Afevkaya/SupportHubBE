4b44ba9b6340a0d121dc09db898a6c362f049db6
afevk <afevkaya5151@gmail.com>
2026-05-18 10:22:32 +0300

    Implement RequestLoggingMiddleware for enhanced HTTP request logging

---
+++ src/Presentation/SupportHub.Api/Middlewares/RequestLoggingMiddleware.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics;
+
+namespace SupportHub.Api.Middlewares;
+
+public class RequestLoggingMiddleware(RequestDelegate next, ILogger<RequestLoggingMiddleware> logger)
+{
+    public async Task InvokeAsync(HttpContext context)
+    {
+        var stopwatch = Stopwatch.StartNew();
+        logger.LogInformation("HTTP {Method} {Path} started", context.Request.Method, context.Request.Path);
+        await next(context);
+        stopwatch.Stop();
+        logger.LogInformation("HTTP {Method} {Path} {StatusCode} in {ElapsedMilliseconds}ms", context.Request.Method, context.Request.Path, context.Response.StatusCode, stopwatch.ElapsedMilliseconds);
+    }
+}
